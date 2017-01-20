3fb6caef1700b3ee924d52b360552ed8e73b04c0
RyanSadowski <nemosaurus@gmail.com>
2017-01-20 02:56:52 +0000

    LCD working too

--- MotoDuino.ino
+++ MotoDuino.ino
@@ -1,4 +1,20 @@
+#include <LiquidCrystal.h>
 #include <Adafruit_NeoPixel.h>
+
+/*
+The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * ArdaFruit to Digital pin 6
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+*/
 
 int sensorPin = A5;    // select the input pin for the potentiometer
 int pixelPin = 6;      // select the pin for the Pixel RBG thing
@@ -6,10 +22,13 @@ int sensorValue = 0;  // variable to store the value coming from the sensor
 int level = 0;
 int pixels = 24;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixels, pixelPin, NEO_GRB + NEO_KHZ800);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 uint32_t color = strip.Color(255, 0, 0);
 
 void setup() {
   strip.begin();
+  lcd.begin(16, 2);
+  lcd.print("Cheaaaa! BOI!");
   strip.setBrightness(64);
   pinMode(pixelPin, OUTPUT);
   Serial.begin(9600);
@@ -52,6 +71,10 @@ uint32_t Wheel(byte WheelPos) {
 }
 
 void werkit(int i) {
+  lcd.setCursor(0,0);
+  lcd.print("420 blazeit MPH");
+  lcd.setCursor(0, 1); //line 2
+  lcd.print(String(i) + " RPM");
   level = 1024 / i;
   checkColor(level);
   for ( int i = 0; i < level; i++) {
